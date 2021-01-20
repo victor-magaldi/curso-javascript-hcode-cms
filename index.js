5951f59c47c9926f2347ec6de8f6a44a80181b37
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-01-20 20:21:55 -0300

    trabalahndo com evento submit

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const fields = document.querySelectorAll("#form-user-create [name]");
 let user = {};
 
-fields.forEach(function (field, index) {
-  if (field.name === "gender") {
-    if (field.checked) {
-      user[field.name] = field.value;
-    }
-  } else {
-    user[field.name] = field.value;
-  }
-});
-console.log(user);
+document
+  .getElementById("form-user-create")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    fields.forEach(function (field, index) {
+      if (field.name === "gender") {
+        if (field.checked) {
+          user[field.name] = field.value;
+        }
+      } else {
+        user[field.name] = field.value;
+      }
+    });
+    console.log(user);
+  });
