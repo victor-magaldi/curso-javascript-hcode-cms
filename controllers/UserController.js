cd72362d16a02a9ee95a938a2fc6f92ac399be51
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-01-22 21:53:12 -0300

    metodo estaticos e formatacao de data

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -74,11 +74,13 @@ class UserController {
     let tr = document.createElement("tr");
     console.log(dataUser.name);
     tr.innerHTML = `
-         <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+         <td><img src="${
+           dataUser.photo
+         }" alt="User Image" class="img-circle img-sm"></td>
          <td>${dataUser.name}</td>
          <td>${dataUser.email}</td>
          <td>${dataUser.admin}</td>
-         <td>${dataUser.register}</td>
+         <td>${Utils.dateFormat(dataUser.register)}</td>
          <td>
          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
          <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
