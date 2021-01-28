23b0cf1688488998f3987f9994239c31f17bef67
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-01-27 21:59:32 -0300

    atualiza números de usuarios  a admin

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -20,7 +20,7 @@ class UserController {
           dataUsers[field.name] = field.value;
         }
       } else if (field.name == "admin") {
-        dataUsers[field.name] = field.checked ? "Sim" : "Não";
+        dataUsers[field.name] = field.checked ? true : false;
       } else {
         dataUsers[field.name] = field.value;
       }
@@ -86,19 +86,35 @@ class UserController {
   }
   addLine(dataUser) {
     let tr = document.createElement("tr");
-    console.log(dataUser.register);
+    tr.dataset.user = JSON.stringify(dataUser);
     tr.innerHTML = `
          <td><img src="${
            dataUser.photo
          }" alt="User Image" class="img-circle img-sm"></td>
          <td>${dataUser.name}</td>
          <td>${dataUser.email}</td>
-         <td>${dataUser.admin}</td>
+         <td>${dataUser.admin ? "sim" : "não"}</td>
          <td>${Utils.dateFormat(dataUser.register)}</td>
          <td>
          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
          <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
          </td>`;
     this.tableEl.appendChild(tr);
+
+    this.updateCount();
+  }
+  updateCount() {
+    let numberUser = 0;
+    let numberAdm = 0;
+
+    const tableLis = [...this.tableEl.children];
+    tableLis.forEach((item) => {
+      numberUser++;
+      const user = JSON.parse(item.dataset.user);
+      console.log(user, user._admin);
+      if (user._admin) numberAdm++;
+    });
+    document.getElementById("number-users").innerText = numberUser;
+    document.getElementById("number-users-adm").innerText = numberAdm;
   }
 }
